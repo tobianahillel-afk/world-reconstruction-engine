f7c1b2bed8072ad4126a5a0c82cb6b1f5592311e
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-09-15 03:27:56 +0200

    [L3.2] Export feature extraction contracts

--- src/wre/reconstruction/__init__.py
+++ src/wre/reconstruction/__init__.py
@@ -7,11 +7,27 @@ from wre.reconstruction.colmap_environment import (
     ColmapEnvironmentIdentity,
     inspect_colmap_environment,
 )
+from wre.reconstruction.colmap_features import (
+    ColmapFeatureExtractionConfig,
+    ColmapFeatureExtractionError,
+    ColmapFeatureExtractionRequest,
+    ColmapFeatureExtractionResult,
+    ColmapFeatureInput,
+    ColmapImageFeatureSummary,
+    extract_colmap_features,
+)
 
 __all__ = [
     "SUPPORTED_COLMAP_VERSION",
     "SUPPORTED_PYCOLMAP_VERSION",
     "ColmapEnvironmentError",
     "ColmapEnvironmentIdentity",
+    "ColmapFeatureExtractionConfig",
+    "ColmapFeatureExtractionError",
+    "ColmapFeatureExtractionRequest",
+    "ColmapFeatureExtractionResult",
+    "ColmapFeatureInput",
+    "ColmapImageFeatureSummary",
+    "extract_colmap_features",
     "inspect_colmap_environment",
 ]
